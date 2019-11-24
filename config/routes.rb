dfebf901648ef39ecc025ae7310dba83f95d6179
Mitch <greenmitchell936@gmail.com>
2019-11-23 19:01:08 -0500

    updates for heroku deployment

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root 'cars#index'
   resources :cars
 end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html end
+
